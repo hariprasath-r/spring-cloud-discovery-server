0299cbb13e27bd4d0c277b081bdd67aaf0c63d09
Hariprasath <balaprasad24@gmail.com>
2020-05-07 11:46:23 +0530

    Initial commit

---
+++ src/main/java/in/hp/boot/springclouddiscoveryserver/SpringCloudDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package in.hp.boot.springclouddiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDiscoveryServerApplication.class, args);
+	}
+
+}
